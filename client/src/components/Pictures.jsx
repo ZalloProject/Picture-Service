1f6a57c9190a415d684bb76489e42325ff62387d
Ben Poling <benpoling@Bens-MacBook-Pro.local>
2019-03-28 13:12:31 -0700

    buttons are on the div to move left/right and functional but still have extra white space

--- client/src/components/Pictures.jsx
+++ client/src/components/Pictures.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
-import posed from 'react-pose';
-import styled from 'styled-components';
 
 // eslint-disable-next-line react/prop-types
 const Pictures = ({ data }) => {
   const divStyle = {
     height: '458px',
-    // width: '900px',
     display: 'flex',
     flexFlow: 'column wrap',
-    // overflow: 'hidden',
   };
   const smallStyle = {
     height: '226px',
@@ -24,8 +20,10 @@ const Pictures = ({ data }) => {
     marginRight: '1px',
   };
   if (data.length > 0) {
-    const first = data.shift();
-    const list = data.reduce((acc, curr) => {
+    const newData = [...data];
+    const first = newData.shift();
+
+    const list = newData.reduce((acc, curr) => {
       acc.push((
         <img key={curr._id} src={curr.url} alt="house" style={smallStyle} />
       ));
@@ -34,7 +32,6 @@ const Pictures = ({ data }) => {
     list.unshift((
       <img key={first._id} src={first.url} style={largeStyle} alt="house" />
     ));
-    console.log(list.length);
     return (
       <div style={divStyle}>
         {list}
@@ -44,22 +41,3 @@ const Pictures = ({ data }) => {
   return null;
 };
 export default Pictures;
-
-// import posed from 'react-pose';
-// import styled from 'styled-components';
-
-// const Box = styled(posed.div({
-//   top: {x: 50},
-//   bottom: {x: 300}
-// }))`
-//   background-color: #f9415d;
-//   position: absolute;
-
-//   ${props => `
-//     height: ${props.size}px;
-//     width: ${props.size}px;
-//     left: calc(50% - ${props.size / 2}px);
-//   `}
-// `;
-
-// export default Box;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable import/extensions */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// eslint-disable-next-line import/extensions
+
 import Pictures from './components/Pictures.jsx';
 import Box from './components/testing.jsx';
+
 
 class App extends Component {
   constructor() {
@@ -11,32 +13,57 @@ class App extends Component {
       data: [],
       pose: 'left',
     };
+    this.leftClick = this.leftClick.bind(this);
+    this.rightClick = this.rightClick.bind(this);
   }
 
   componentWillMount() {
     fetch('http://localhost:3000/links', {
       method: 'GET',
     }).then(res => res.json())
-      .then((data) => {
+      .then((newData) => {
         this.setState({
-          data,
+          data: newData,
         });
       });
+  }
+
+  leftClick() {
+    this.setState({
+      pose: 'left',
+    });
+  }
+
+  rightClick() {
+    this.setState({
+      pose: 'right',
+    });
   }
 
   render() {
     const divStyle = {
       height: '458px',
       width: '962px',
-      overflow: 'scroll',
+      overflow: 'hidden',
     };
     const { data, pose } = this.state;
+    const leftButtonStyle = {
+      position: 'fixed',
+      top: '225px',
+    };
+    const rightButtonStyle = {
+      position: 'fixed',
+      top: '225px',
+      left: '850px',
+    };
     return (
 
       <div style={divStyle}>
+        <button type="submit" onClick={this.leftClick} style={leftButtonStyle}>left</button>
         <Box pose={pose}>
           <Pictures data={data} />
         </Box>
+        <button type="submit" onClick={this.rightClick} style={rightButtonStyle}>right</button>
       </div>
 
     );
